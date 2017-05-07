84dd8556a45d88571049b8630be423f3d87ec726
Robert Bultman <robertbultman@Roberts-MacBook-Pro.local>
2017-05-07 08:25:13 -0400

    Added more LEDs to blink.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+DigitalOut ledOrange(LED3);
+DigitalOut ledGreen(LED4);
+DigitalOut ledRed(LED5);
+DigitalOut ledBlue(LED6);
+
+Serial console(SERIAL_TX, SERIAL_RX, 115200);
+
+Timer millis;
 
 // main() runs in its own thread in the OS
 // (note the calls to wait below for delays)
 int main() {
-    while (true) {
-        led1 = !led1;
-        wait(0.5);
-    }
+   uint32_t oldTime;
+   uint32_t newTime;
+   console.printf("Hello, world.\r\n");
+   millis.start();
+   oldTime = millis.read();
+   newTime = oldTime;
+
+   while (true) {
+      newTime = millis.read();
+      if (newTime != oldTime) {
+         oldTime = newTime;
+         if (console.writeable()) {
+            ledOrange = !ledOrange;
+         }
+         ledGreen = !ledGreen;
+         ledRed = !ledRed;
+         ledBlue = !ledBlue;
+
+         console.printf("Tick %lu\r\n", millis.read());
+         console.putc('R');
+      }
+   }
 }
 
